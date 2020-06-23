aacb6e4b7ff95a5955880cdd15fd87be39cd1ea2
kayaondul <kayaondul@gmail.com>
2020-06-23 16:58:39 +0300

    database initiated

---
+++ bulletin599/variables.py
@@ -0,0 +1,19 @@
+# settings.py imports variables-template.py for the variables below
+# copy and rename this file as variables-template.py and add database credentials and static file paths
+secret_key = '3uy@fmaq+cwei^w$pv-7o5co56#0)1ll$)^yfup)8$(o%p%knz'
+
+db_name = 'swe'
+db_host = ''
+db_user = ''
+db_password = ''
+db_port = ''
+
+static_root = ''
+staticfiles_dirs = (
+    '',
+)
+media_root = ''
+
+allowed_hosts = []
+
+sentry_dsn = "https://156fe2359a8b4c20b4d7e62a6c3e6e32@sentry.io/4334561"
