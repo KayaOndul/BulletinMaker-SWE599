8c8a8c72117b01a807000e58349022d7520afe3b
kayaondul <kayaondul@gmail.com>
2020-06-24 05:45:27 +0300

    changed paths added cors headers config

--- api/urls.py
+++ api/urls.py
@@ -5,14 +5,17 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from api import views
 
 urlpatterns = [
-    path('register/', views.AuthViews.register, name="register"),
-    path('login/', views.AuthViews.login, name="login"),
-    path('logout/', views.AuthViews.logout, name="logout"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path(r'users/', include([
-        path('', views.UserViews.getAll),
-        path(r'<str:username>', views.UserViews.getUser),
+    path('api/', include([
+        path('register', views.AuthViews.register, name="register"),
+        path('login', views.AuthViews.login, name="login"),
+        path('logout', views.AuthViews.logout, name="logout"),
+        path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+        path("refresh", TokenRefreshView.as_view(), name="token_refresh"),
+        path('users/', include([
+            path('', views.UserViews.getAll),
+            path(r'<str:username>', views.UserViews.getUser),
 
+        ]))
     ]))
+
 ]
